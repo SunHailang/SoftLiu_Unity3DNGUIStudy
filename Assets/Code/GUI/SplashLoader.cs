abf5e03cd087da8fe8d4b2f292cd5c9b465db717
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-04-13 10:42:13 +0800

    update scenes

---
+++ Assets/Code/GUI/SplashLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashLoader : MonoBehaviour
+{
+
+    [SerializeField]
+    private string m_image = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate<GameObject>(Resources.Load<GameObject>(m_image), transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
