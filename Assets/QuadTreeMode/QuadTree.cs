83b2d4d0e1a9c85829445d0f00171fbce44227fe
SunHailang <hai-lang.sun@ubisoft.com>
2020-05-16 17:29:27 +0800

    add Quad tree test

---
+++ Assets/QuadTreeMode/QuadTree.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuadTree
+{
+
+    public Rectangle boundary;
+    public int capacity;
+    public Queue<Point> points;
+
+    public QuadTree northeast;
+    public QuadTree northwest;
+    public QuadTree southeast;
+    public QuadTree southwest;
+
+    public bool divided;
+    public QuadTree(Rectangle boundary, int n)
+    {
+        this.boundary = boundary;
+        this.capacity = n;
+        this.points = new Queue<Point>();
+        this.divided = false;
+    }
+
+    private void subdivde()
+    {
+        float x = this.boundary.x;
+        float y = this.boundary.y;
+        float w = this.boundary.w;
+        float h = this.boundary.h;
+        Rectangle ne = new Rectangle(x + w / 4, y + h / 4, w / 2, h / 2);
+        this.northeast = new QuadTree(ne, this.capacity);
+        Rectangle nw = new Rectangle(x - w / 4, y + h / 4, w / 2, h / 2);
+        this.northwest = new QuadTree(nw, this.capacity);
+        Rectangle se = new Rectangle(x + w / 4, y - h / 4, w / 2, h / 2);
+        this.southeast = new QuadTree(se, this.capacity);
+        Rectangle sw = new Rectangle(x - w / 4, y - h / 4, w / 2, h / 2);
+        this.southwest = new QuadTree(sw, this.capacity);
+        this.divided = true;
+    }
+
+    public bool insert(Point point)
+    {
+        if (!this.boundary.contains(point)) return false;
+
+        if (this.points.Count < this.capacity)
+        {
+            this.points.Enqueue(point);
+            return true;
+        }
+        else
+        {
+            if (!divided)
+            {
+                this.subdivde();
+            }
+            if (this.northeast.insert(point)) return true;
+            else if (this.northwest.insert(point)) return true;
+            else if (this.southeast.insert(point)) return true;
+            else if (this.southwest.insert(point)) return true;
+            else
+                return false;
+        }
+    }
+
+    public Queue<Point> query(Rectangle range, Queue<Point> found = null)
+    {
+        if (found == null) found = new Queue<Point>();
+
+        if (!this.boundary.intersects(range))
+        {
+            return found;
+        }
+        else
+        {
+            IEnumerator ie = this.points.GetEnumerator();
+            while (ie.MoveNext())
+            {
+                Point p = ie.Current as Point;
+                if (p != null && range.contains(p))
+                {
+                    found.Enqueue(p);
+                }
+            }
+            if (this.divided)
+            {
+                this.northwest.query(range, found);
+                this.northeast.query(range, found);
+                this.southwest.query(range, found);
+                this.southeast.query(range, found);
+            }
+        }
+        return found;
+    }
+
+
+    public void Show()
+    {
+        IEnumerator ie = this.points.GetEnumerator();
+        while (ie.MoveNext())
+        {
+            Point p = (Point)ie.Current;
+            if (p != null)
+                p.data.color = Color.white;
+        }
+
+        Debug.DrawLine(this.boundary.leftDown, this.boundary.leftUp, Color.green);
+        Debug.DrawLine(this.boundary.leftDown, this.boundary.rightDown, Color.green);
+        Debug.DrawLine(this.boundary.leftUp, this.boundary.rightUp, Color.green);
+        Debug.DrawLine(this.boundary.rightDown, this.boundary.rightUp, Color.green);
+        if (this.divided)
+        {
+            this.northeast.Show();
+            this.northwest.Show();
+            this.southeast.Show();
+            this.southwest.Show();
+        }
+    }
+}
+
+/// <summary>
+/// 假设点是带有碰撞盒的
+/// </summary>
+public class Point
+{
+    public float x;
+    public float y;
+    /// <summary>
+    /// BoxCollider的大小
+    /// </summary>
+    public float w = 0;
+    public float h = 0;
+
+    private Image m_data;
+    public Image data { get { return m_data; } }
+    public Point(float x, float y, Image data)
+    {
+        this.x = x;
+        this.y = y;
+        this.m_data = data;
+        // 初始化为 0
+        //this.w = 0.01851845f;
+        //this.h = 0.01851845f;
+    }
+}
+
+public class Rectangle
+{
+    public float x;
+    public float y;
+    public float w;
+    public float h;
+
+    // 得到4个顶点的坐标
+    public Vector2 leftDown;
+    public Vector2 leftUp;
+    public Vector2 rightDown;
+    public Vector2 rightUp;
+
+    public Rectangle(float x, float y, float w, float h)
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+
+        this.leftDown = new Vector2(this.x - w / 2, this.y - h / 2);
+        this.leftUp = new Vector2(this.x - w / 2, this.y + h / 2);
+        this.rightDown = new Vector2(this.x + w / 2, this.y - h / 2);
+        this.rightUp = new Vector2(this.x + w / 2, this.y + h / 2);
+
+    }
+    /// <summary>
+    /// 带BoxCollider的大小  假设 BoxCollider的中心点和物体本身重合
+    /// </summary>
+    /// <param name="point"></param>
+    /// <returns></returns>
+    public bool contains(Point point)
+    {
+        return ((point.x + point.w / 2) >= (this.x - this.w / 2) &&
+            (point.x - point.w / 2) <= (this.x + this.w / 2) &&
+            (point.y + point.h / 2) >= (this.y - this.h / 2) &&
+            (point.y - point.h / 2) <= (this.y + this.h / 2));
+    }
+
+    public bool intersects(Rectangle range)
+    {
+        return !(range.x - range.w / 2 > this.x + this.w / 2 ||
+            range.x + range.w / 2 < this.x - this.w / 2 ||
+            range.y - range.h / 2 > this.y + this.h / 2 ||
+            range.y + range.h / 2 < this.y - this.h / 2);
+    }
+}
+
---
+++ Assets/QuadTreeMode/QuadTreeTest.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuadTreeTest : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_enemyParent = null;
+    [SerializeField]
+    private Transform m_enemyPrefab = null;
+    [SerializeField]
+    private Camera m_camera = null;
+
+    private QuadTree m_tree = null;
+
+    private float m_height = 0.2f;
+    private float m_width = 0.2f;
+    private Rectangle m_range = null;
+
+    private void Start()
+    {
+
+        Vector3 test = m_camera.ScreenToWorldPoint(new Vector3(10, 0, 0));
+        Vector3 zo = m_camera.ScreenToWorldPoint(Vector3.zero);
+        Debug.Log(test.x - zo.x);
+
+        //return;
+
+        Vector3 start = new Vector3(520, 115);
+        Vector3 startWorld = m_camera.ScreenToWorldPoint(start);
+        Vector3 endWorld = m_camera.ScreenToWorldPoint(new Vector3(start.x + 1300, start.y + 850, 0));
+        Debug.Log(startWorld + " -> " + endWorld);
+        float w = endWorld.x - startWorld.x;
+        float h = endWorld.y - startWorld.y;
+        Rectangle rect = new Rectangle(startWorld.x + w / 2, startWorld.y + h / 2, w, h);
+
+
+        m_tree = new QuadTree(rect, 4);
+
+        for (int i = 0; i < 500; i++)
+        {
+            Transform obj = GameObject.Instantiate<Transform>(m_enemyPrefab, m_enemyParent);
+            obj.name = string.Format("enemy" + i);
+            float x = UnityEngine.Random.Range(-600, 600);
+            float y = UnityEngine.Random.Range(-400, 400);
+            Vector3 pos = new Vector3(x, y, 0);
+            obj.localPosition = pos;
+            //obj.position = m_camera.ScreenToWorldPoint(pos);
+            //Debug.Log(obj.position);
+            m_tree.insert(new Point(obj.position.x, obj.position.y, obj.GetComponent<Image>()));
+        }
+        Debug.Log("Insert End");
+
+        m_range = new Rectangle(startWorld.x - m_width / 2, startWorld.y - m_height / 2, m_width, m_height);
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void Update()
+    {
+
+        //float horizontal = Input.GetAxis("Horizontal"); //获取垂直轴
+        //float vertical = Input.GetAxis("Vertical");    //获取水平轴
+        //m_range.y += vertical * Time.deltaTime;
+        //m_range.x += horizontal * Time.deltaTime;
+
+        Vector3 start = new Vector3(520, 115);
+        Vector3 leftDown = m_camera.ScreenToWorldPoint(start);
+        Vector3 leftUp = m_camera.ScreenToWorldPoint(new Vector3(start.x, start.y + 850));
+        Vector3 rightDown = m_camera.ScreenToWorldPoint(new Vector3(start.x + 1300, start.y));
+        Vector3 rightUp = m_camera.ScreenToWorldPoint(new Vector3(start.x + 1300, start.y + 850));
+
+        Debug.DrawLine(leftDown, leftUp, Color.red);
+        Debug.DrawLine(leftDown, rightDown, Color.red);
+        Debug.DrawLine(leftUp, rightUp, Color.red);
+        Debug.DrawLine(rightDown, rightUp, Color.red);
+
+        m_tree.Show();
+
+        Vector3 mouseWorld = m_camera.ScreenToWorldPoint(Input.mousePosition);
+
+        m_range.y = mouseWorld.y;
+        m_range.x = mouseWorld.x;
+
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Debug.Log(m_range.x + " -> " + m_range.y);
+        Queue<Point> points = m_tree.query(m_range);
+        //Debug.Log(points.Count);
+        IEnumerator ie = points.GetEnumerator();
+        while (ie.MoveNext())
+        {
+            Point p = (Point)ie.Current;
+            if (p != null)
+                p.data.color = Color.red;
+        }
+        //}
+
+        Vector3 leftDownRange = new Vector3(m_range.x - m_range.w / 2, m_range.y - m_range.h / 2);
+        Vector3 leftUpRange = new Vector3(m_range.x - m_range.w / 2, m_range.y + m_range.h / 2);
+        Vector3 rightDownRange = new Vector3(m_range.x + m_range.w / 2, m_range.y - m_range.h / 2);
+        Vector3 rightUpRange = new Vector3(m_range.x + m_range.w / 2, m_range.y + m_range.h / 2);
+        Debug.DrawLine(leftDownRange, leftUpRange, Color.blue);
+        Debug.DrawLine(leftDownRange, rightDownRange, Color.blue);
+        Debug.DrawLine(leftUpRange, rightUpRange, Color.blue);
+        Debug.DrawLine(rightDownRange, rightUpRange, Color.blue);
+        //m_tree.query()
+
+    }
+}
